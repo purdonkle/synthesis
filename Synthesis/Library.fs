08097dc1240abdea40991444b3f9f6a17caba41e
Adam Purdon <G18P8803@ict.ru.ac.za>
2020-03-12 16:51:18 +0200

    lab work, half way

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -1,31 +1,54 @@
 ﻿module Synthesis
 
-let abelar _ =
-    failwith "Not implemented"
+let abelar input = 
+    (input > 12 && input < 3097) && (input % 12 = 0)
+    //failwith "Not implemented"
 
-let area _ _ =
-    failwith "Not implemented"
+let area b h =
+    match (b < 0.0 || h < 0.0) with
+    | true -> failwith "base of height is negative"
+    | _ -> (0.5 * b) * h
+    //failwith "Not implemented"
 
-let zollo _ =
-    failwith "Not implemented"
+let zollo input =
+    match (input < 0) with
+    | true -> input * -1
+    | _ -> input * 2
+    //failwith "Not implemented"
 
-let min _ _ =
-    failwith "Not implemented"
+let min a b =
+    match (a < b) with
+    | true -> a
+    | _ -> b
+    //failwith "Not implemented"
 
-let max _ _ =
-    failwith "Not implemented"
+let max a b =
+    match (a < b) with
+    | false -> a
+    | _ -> b
+    //failwith "Not implemented"
 
-let ofTime _ _ _ =
-    failwith "Not implemented"
+let ofTime h m s = 
+    (h * 3600) + (m * 60) + s
+    //failwith "Not implemented"
 
-let toTime _ =
-    failwith "Not implemented"
+let toTime seconds =
+    match (seconds < 0) with
+    | true -> (0,0,0)
+    | _ -> ((seconds / 60) / 60, (seconds / 60) % 60, seconds % 60)
+    //failwith "Not implemented"
 
-let digits _ =
-    failwith "Not implemented"
+let digits input =
+    let rec count n =
+        match (n < 10 && n > -10) with
+        | true -> 1
+        | _ -> 1 + count(n/10)
+    count input
+    //failwith "Not implemented"
 
-let minmax _ =
-    failwith "Not implemented"
+let minmax input =
+    (min(min(), min()), max(max()))
+    //failwith "Not implemented"
 
 let isLeap _ =
     failwith "Not implemented"
